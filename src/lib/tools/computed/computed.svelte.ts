239ce757d5e700b8b70bb44f076a419ed8b7c43b
dualjack <jakubkuranda@gmail.com>
2025-05-12 02:20:38 +0200

    Add: Computed options.

--- src/lib/tools/computed/computed.svelte.ts
+++ src/lib/tools/computed/computed.svelte.ts
@@ -1,8 +1,19 @@
+type ComputedOptions = {
+    debounceDelay?: number,
+}
+
 export interface Computed<T> {
     get current(): T
 }
 
-export function computed<T>(cb: () => T): Computed<T>{
+export function computed<T>(cb: () => T, options?: ComputedOptions): Computed<T>{
+
+    const {
+        debounceDelay = 0,
+    } = options ?? {};
+
+    //  TODO - debounce logic.
+
     let derived = $derived.by(cb);
     return {
         get current(){ return derived }
